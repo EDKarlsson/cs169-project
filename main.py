8eec741998b8837653b7823c4c499f00fecffe90
BSchrift <thinsos@gmail.com>
2017-12-01 16:41:13 -0800

    added relaxing constraints for minimum number of hours

--- main.py
+++ main.py
@@ -41,29 +41,44 @@ pay_vec = np.array([pay[worker] for worker in workers])
 print(pay_vec)
 
 hours = np.array([[model.addVar(vtype=GRB.BINARY,name='x_{' + str(i) + ',' + str(j) + '}') for j in range(m)] for i in range(n)])
+removable_constrs = []
 for i in range(n):
     for j in range(m):
         if A[i,j] == 0:
             model.addConstr(hours[i,j] == 0)
 
+
 for col in hours.T:
-    model.addConstr(quicksum(col) >= 1) # only one per shift
+    model.addConstr(quicksum(col) == 1) # only one per shift
 
 model.addConstr(quicksum(hours.ravel()) >= m) # all 12 shifts covered
+
+row_max_hours = [2 for row in hours]
+for i,row in enumerate(hours):
+    removable_constrs.append(model.addConstr(quicksum(row) <= row_max_hours[i], name=str(i))) # only 3 hour shifts allowed per person
+
 for i in range(n):
     for j in range(m - 1):
         # we want (hours[i,j] == 0 and (hours[i,j + 1] == 0 or quicksum(hours[i,:j]) == 0))
         #          or hours[i,j] == 1) but we cant explicitly program logical constraints
         helper = model.addVar()
-        model.addConstr(helper == hours[i,j + 1] * quicksum(hours[i,:j])) # if helper == 1 then either hours[i,j+1] is zero or all the previous hours are zero
+        model.addConstr(helper == hours[i,j + 1] * quicksum(hours[i,:j])) # if helper == 0 then either hours[i,j+1] is zero or all the previous hours are zero
         model.addConstr((hours[i,j] == 0) >> (helper == 0)) # if hour j is a zero then hour j+1 can only be a 1 if all the previous are zero
-
 
 
 model.setObjective(quicksum(np.dot(hours.T, pay_vec).ravel()), GRB.MINIMIZE)
 
 model.update()
 model.optimize()
+
+removed = []
+
+
+while model.status == GRB.status.INFEASIBLE:
+    # model.computeIIS()
+    model.feasRelaxS(1, True, False, True)
+    model.update()
+    model.optimize()
 
 results = np.ndarray(shape=(n,m))
 for i in range(n):
